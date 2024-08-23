8b8de571e1a978d5457b5cafa20bad39f5c4db66
Deivid Ribeiro <deividlsribeiro@gmail.com>
2024-08-23 13:50:24 -0400

    Update docstring to blazar_clean.py.

--- bjet_mcmc/blazar_clean.py
+++ bjet_mcmc/blazar_clean.py
@@ -15,7 +15,7 @@ __all__ = ["blazar_clean"]
 
 def clean(data=True, data_folder=None, parameter_files=True, parameter_folder=None):
     """
-    Remove data files and remove parameter files not named params.txt
+    Clean function documentation. Remove data files and remove parameter files not named params.txt
 
     Args:
         data (optional): bool
@@ -28,9 +28,18 @@ def clean(data=True, data_folder=None, parameter_files=True, parameter_folder=No
             Relative path to parameter folder; default is None, will be set to
             PARAMETER_FOLDER
 
-    Returns:
-        bool
-        success: Returns if both the data_folder and the parameter_folder exists.
+
+    :param data: A boolean indicating whether to clean data files. Default is True.
+    :type data: bool
+    :param data_folder: The folder where the data files are stored. If None, the default data folder will be used.
+    :type data_folder: str or None
+    :param parameter_files: A boolean indicating whether to clean parameter files. Default is True.
+    :type parameter_files: bool
+    :param parameter_folder: The folder where the parameter files are stored. If None, the default parameter folder will be used.
+    :type parameter_folder: str or None
+    :return: A boolean indicating whether the clean operation was successful.
+    :rtype: bool
+
     """
     success = True
     if data:
