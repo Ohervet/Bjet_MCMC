da588407b06578f4aea3e5fac04e12aa54dbf2ce
Deivid Ribeiro <deividlsribeiro@gmail.com>
2024-09-05 15:47:15 -0400

    Found autodoc_mock_imports to avoid having to install libraries (like emcee). Started a main list.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ pygments_style = "sphinx"
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autodoc_mock_imports = ["numpy", "emcee", "scipy", "astropy", "tqdm", "matplotlib"]
 
 # -- Options for HTML output
 
